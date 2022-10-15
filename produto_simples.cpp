e7702498a41f0f49bb6d4927b08276621552e98d
ROBIMgamert <115877317+ROBIMgamert@users.noreply.github.com>
2022-10-15 09:24:27 -0300

    Create produto_simples.cpp

---
+++ produto_simples.cpp
@@ -0,0 +1,12 @@
+#import <math.h>
+#import <iostream>
+using namespace std;
+
+int main() {
+  int a,b,prod;
+  cin >> a;
+  cin >> b;
+  prod = a * b;
+  cout << "PROD = " << prod << endl;
+  return 0;
+  }
